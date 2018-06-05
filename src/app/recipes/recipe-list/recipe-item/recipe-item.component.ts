c674532c70b0f554e83d283498dc19eee5b0ad8e
gnappo1 <gnappo.1@live.it>
2018-06-04 18:29:08 -0700

    Now each recipe is clickable and on click, the recipe-detil section will know what data to output. I used EventEmitter and then binded the event passing the necessary data to the parent component, until recipes.componet is reached ( there I have my <app-recipe-detail> tag).

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,10 +6,16 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
+  @Output() recipeSelected = new EventEmitter<void>();
   @Input() recipe: Recipe;
+  
   constructor() { }
 
   ngOnInit() {
+  }
+
+  recipeClicked() {
+    this.recipeSelected.emit();
   }
 
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -8,11 +8,18 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyW__esGEwn6kZHgFk50I5ze-Vso6E40SxPARBgWFkcg7h1ION')
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyW__esGEwn6kZHgFk50I5ze-Vso6E40SxPARBgWFkcg7h1ION'),
+    new Recipe('Second Test', 'Another simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyW__esGEwn6kZHgFk50I5ze-Vso6E40SxPARBgWFkcg7h1ION')
   ];
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeClicked(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
